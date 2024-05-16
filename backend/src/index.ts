514dd6f64104697d84520777b73be910462eaaee
Sumit <sumitgarg380@gmail.com>
2024-05-16 20:05:20 +0530

    template routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,22 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+  return c.text('signup')
 })
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('signup')
+})
+app.post('/api/v1/blog', (c) => {
+  return c.text('signup')
+})
+app.put('/api/v1/blog', (c) => {
+  return c.text('signup')
+})
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('signup')
+})
+
 
 export default app
