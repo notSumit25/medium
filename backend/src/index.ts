25ed2c7893a39399e9bae1cb9b64f69680c341f5
Sumit <sumitgarg380@gmail.com>
2024-05-16 23:25:56 +0530

    signin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,27 @@ const app = new Hono<{
   }
 }>()
 
-
+app.use('/api/v1', async (c,next) => {
+  const response = c.req.header('authorization')
+  if (!response) {
+    c.status(401)
+    return c.json({ message: 'Unauthorized' });
+  }
+  const token = response.split(' ')[1]
+  try {
+    const payload = await verify(token, 'secret')
+    if(payload.id){
+      next()
+    }else{
+      c.status(401)
+      return c.json({ message: 'Unauthorized' });
+    } 
+  } catch (e) {
+    c.status(401)
+    return c.json({ message: 'Unauthorized' });
+  }
+}
+)
 
 app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
@@ -23,15 +43,41 @@ app.post('/api/v1/signup', async (c) => {
       password: body.password,
     }
   })
-  const token = sign({ id: user.id}, 'secret')
+  const token = await sign({ id: user.id}, 'secret')
   return c.json({ jwt: token});
 })
 
-app.post('/api/v1/signin', (c) => {
-  return c.text('signup')
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    }
+  })
+  if (!user) {
+    c.status(403)
+    return c.json({ message: 'Invalid email or password' });
+  }
+  const jwt = await sign({ id: user.id}, 'secret');
+  return c.json({ jwt });
 })
-app.post('/api/v1/blog', (c) => {
-  return c.text('signup')
+app.post('/api/v1/blog', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  const res = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: body.authorId,
+    }
+  })
+  return c.json(res);
 })
 app.put('/api/v1/blog', (c) => {
   return c.text('signup')
