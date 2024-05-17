27e29570afb24836eb4ab4945d526e84ea644a74
Sumit <sumitgarg380@gmail.com>
2024-05-17 19:51:30 +0530

    frontend almost finalised

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
-import Blog from "./pages/Blog";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import  Signup  from './pages/Signup'
+import  Signin  from './pages/Signin'
+import { Blog } from './pages/Blog'
+import { Blogs } from "./pages/Blogs";
+import { Publish } from './pages/Publish';
 
 function App() {
+
   return (
     <>
       <BrowserRouter>
@@ -11,10 +14,12 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
