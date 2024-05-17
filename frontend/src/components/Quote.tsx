8947b36547c921dc764ad5746225b8aa7c4ec631
Sumit <sumitgarg380@gmail.com>
2024-05-17 19:44:15 +0530

    frontend-progress

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,17 @@
+export default function Quote() {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+          <div className="h-full text-2xl font-bold text-justify w-full">
+            "The Customer Support I recieved was amazing. I would recommend this
+            to anyone who is looking for a easy to use and affordable solution
+            to their problems."
+          </div>
+          <div className="max-w-lg text-xl font-bold">Julius WinField</div>
+          <div className="max-w-lg text-sm font-light">CEO, Initech</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Auth from "../components/Auth";
+import Quote from "../components/Quote";
 
 export default function Signup() {
   return (
-    <div>
-      signup
+    <div className="grid grid-cols-2">
+      <div>
+        <Auth />
+      </div>
+      <div className="invisible lg:visible">
+        <Quote />
+      </div>
     </div>
-  )
+  );
 }
